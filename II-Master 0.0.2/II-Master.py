29990817f73a94d0c7ae397c47465defa445b169
xxcy235 <475144628@qq.com>
2020-06-15 10:18:29 +0800

    Fisrt Commit

---
+++ II-Master 0.0.2/II-Master.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+import os
+from IIMasterExploit.lib.command import Console_command
+from IIMasterExploit.lib.common import *
+
+
+def main():
+
+	Iiisc_cmd = Console_command()
+	Iiisc_cmd.start()
+
+if __name__ == "__main__":
+	main()
